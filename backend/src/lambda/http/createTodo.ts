82372dbb123cfb52b6e9a7f8f70322c14285de8f
luancaius <luancaius@outlook.com>
2020-05-24 23:15:26 -0400

    feat: adding getTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,11 @@
-import 'source-map-support/register'
+// import 'source-map-support/register'
+// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+// //import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   //const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+//   return undefined
+// }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,39 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk'
+import { APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const XAWS = AWSXRay.captureAWS(AWS)
+const docClient = createDynamoDBClient()
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+const todosTable = process.env.TODOS_TABLE
+//const indexName = process.env.INDEX_NAME
+
+export const handler: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
+
+
+  const result = await docClient.scan({
+    TableName: todosTable
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+}
+
+function createDynamoDBClient() {
+  console.log(process.env);
+
+  if (process.env.IS_OFFLINE) {
+    console.log('creating dynamodb instance');
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+  return new XAWS.DynamoDB.DocumentClient()
 }
