e2372deadd75afcb9bf74c7dc03c76abbcbaa366
luancaius <luancaius@outlook.com>
2020-05-25 01:20:58 -0400

    feat: create todos

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,11 +1,54 @@
-// import 'source-map-support/register'
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-// //import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import { getUserId } from "../utils";
+import * as AWSXRay from 'aws-xray-sdk'
 
+const XAWS = AWSXRay.captureAWS(AWS)
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   //const newTodo: CreateTodoRequest = JSON.parse(event.body)
+const docClient = createDynamoDBClient()
 
+const table = process.env.TODOS_TABLE
 
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+  const timestamp = new Date().toISOString();
+  const todoId = uuidv4()
+  const userId = getUserId(event)
+
+  const newItem = {
+    todoId,
+    userId,
+    timestamp,
+    ...newTodo
+  }
+
+  console.log(newItem);
+
+  await docClient.put({
+    TableName: table,
+    Item: newItem
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ newItem }),
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+}
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('creating dynamodb instance');
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+  return new XAWS.DynamoDB.DocumentClient()
+}
--- backend/src/lambda/utils.ts
+++ backend/src/lambda/utils.ts
@@ -8,7 +8,12 @@ import { parseUserId } from "../auth/utils";
  * @returns a user id from a JWT token
  */
 export function getUserId(event: APIGatewayProxyEvent): string {
+
   const authorization = event.headers.Authorization
+  console.log(authorization);
+
+  if (event.headers)
+    return "1";
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
