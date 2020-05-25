097734313daccad208ed2f842e3049ee70c381fc
luancaius <luancaius@outlook.com>
2020-05-25 02:04:01 -0400

    feat: adding update and delete

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,9 +1,53 @@
-// import 'source-map-support/register'
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   //const todoId = event.pathParameters.todoId
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = createDynamoDBClient()
+
+const table = process.env.TODOS_TABLE
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('creating dynamodb instance');
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+  return new XAWS.DynamoDB.DocumentClient()
+}
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+
+  console.log('deleting todoId ' + todoId);
+
+  const item = await docClient.getItem({
+    TableName: table,
+    Key: {
+      todoId: todoId
+    }
+  }).promise();
 
 
-//   return undefined
-// }
+  await docClient.delete({
+    TableName: table,
+    Key: {
+      todoId: todoId,
+      timestamp: item.timestamp
+    }
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: `${todoId} deleted`,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+
+  return undefined
+}
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,55 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk';
+import { getUserId } from "../utils";
+import * as AWSXRay from 'aws-xray-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+const XAWS = AWSXRay.captureAWS(AWS)
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+const docClient = createDynamoDBClient()
+
+const table = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  const timestamp = new Date().toISOString();
+  const userId = getUserId(event)
+
+  const newItem = {
+    todoId,
+    userId,
+    timestamp,
+    ...updatedTodo
+  }
+
+  console.log(newItem);
+
+  await docClient.update({
+    TableName: table,
+    Item: newItem
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ newItem }),
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  };
+}
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('creating dynamodb instance');
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+  return new XAWS.DynamoDB.DocumentClient()
+}
