9ff9b07c8cf7a9cefee0736e9e0ba5bc8866f1da
luancaius <luancaius@outlook.com>
2020-05-28 14:39:41 -0400

    fix: updating auth to get jwk

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,38 +1,15 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify } from 'jsonwebtoken'
-//import { verify, decode } from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-//import Axios from 'axios'
-//import { Jwt } from '../../auth/Jwt'
+import Axios from 'axios'
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = '...'
-const cert = `-----BEGIN CERTIFICATE-----
-MIIDATCCAemgAwIBAgIJLAKSngy3oYduMA0GCSqGSIb3DQEBCwUAMB4xHDAaBgNV
-BAMTE2x1YW4tdGVzdC5hdXRoMC5jb20wHhcNMjAwNTA5MTQwMjIwWhcNMzQwMTE2
-MTQwMjIwWjAeMRwwGgYDVQQDExNsdWFuLXRlc3QuYXV0aDAuY29tMIIBIjANBgkq
-hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuD3F4zWHpUuGbDLmM3X1950we5Ato9K2
-WQAMwU4R/UxPq6wfzE/NEUuTsSGU3BWH6MG1PlGv3K/HaLQmh/T/hWWMWjgSTYTV
-KudfEV/j5+iwvu9D+BNMvBC5+U2bKTZmOnQmy2qpHQGFQexL6+oYfCjrGeO5Bf7+
-z1XRGEERsAabH5MDcvQ8tfj9HAcl3mB6WjTpo/0CJpnY4WGf/9E6BxZtIvFYpYA8
-Gh90XxA1BMvhU3LjnxPIKU60DGz+AClGNDJUcfgLTBn/XxvX6iDRi5g3Wrbvj5bA
-6oCSTmoOUYd+hIfNGAOVt5/dwC7nxDaq8L5rBDrZCjmj6Y09vzyquwIDAQABo0Iw
-QDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBS261Y4KhbN7yi1kOr5DhldYbJa
-/jAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEBAJSGn9TEjlErWQL1
-6TLjI4FyRxHwnEcgjZj7eIl/cjzDoq+F8QyZsNtH0oxjG2EWQF6KCH+QW2JJesTj
-/I9L8RoKWh5b7N36JIuGAohhzlK+q548LlYjXgtmeW/oJAirE+Sqx+h2YractSE3
-8eMecFB3euwqzrxRGfioOO7PkQviHFzT/hf2JlpJ2OeUUMGFtDgNr4vdTuyFnG9n
-XRCzuceLQE29CdK1puinCJNY0+oiz1bX+dQOKDFI/SupGL8Kb+PWxZnmP+0VcALo
-0Yi3yEL2zrAHeSYNa4I6tPdcm2AAPqkBSdEkhhlqTVEOik52s0KrOjD9I5J31LnO
-v+VZcCw=
------END CERTIFICATE-----`
+const jwksUrl = 'https://luan-test.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
     event: CustomAuthorizerEvent
@@ -73,19 +50,36 @@ export const handler = async (
         }
     }
 }
-
-// async function verifyToken(authHeader: string): Promise<JwtPayload> {
-//     const token = getToken(authHeader)
-//     const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-//     // TODO: Implement token verification
-//     // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-//     // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-//     return undefined
-// }
-
-
-function verifyToken(authHeader: string): JwtPayload {
+const getSigningKey = async (jwkurl, kid) => {
+    let res = await Axios.get(jwkurl, {
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            'Access-Control-Allow-Credentials': true,
+        }
+    });
+    let keys = res.data.keys;
+    // since the keys is an array its possible to have many keys in case of cycling.
+    const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+        && key.kty === 'RSA' // We are only supporting RSA
+        && key.kid           // The `kid` must be present to be useful for later
+        && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
+    ).map(key => {
+        return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+    const signingKey = signingKeys.find(key => key.kid === kid);
+    if (!signingKey) {
+        throw new Error('Invalid signing keys')
+    }
+    logger.info("Signing keys created successfully ", signingKey)
+    return signingKey
+};
+function certToPEM(cert) {
+    cert = cert.match(/.{1,64}/g).join('\n');
+    cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+    return cert;
+}
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
     if (!authHeader)
         throw new Error('No auth header')
 
@@ -94,18 +88,8 @@ function verifyToken(authHeader: string): JwtPayload {
 
     const split = authHeader.split(' ')
     const token = split[1]
+    const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-    return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+    let key = await getSigningKey(jwksUrl, jwt.header.kid)
+    return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
 }
-
-// function getToken(authHeader: string): string {
-//     if (!authHeader) throw new Error('No authentication header')
-
-//     if (!authHeader.toLowerCase().startsWith('bearer '))
-//         throw new Error('Invalid authentication header')
-
-//     const split = authHeader.split(' ')
-//     const token = split[1]
-
-//     return token
-// }
